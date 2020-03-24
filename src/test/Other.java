fd56b2003d82f11087e803e80d691ad113fb875c
mutongdavid <mutong_david@163.com>
2020-03-24 20:42:55 +0800

    add Other.java and Test.java

---
+++ src/test/Other.java
@@ -0,0 +1,9 @@
+package test;
+
+/**
+ * description:描述
+ * author:mutong(david)
+ * time:19:40
+ */
+public class Other {
+}
---
+++ src/test/Test.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * description:描述
+ * author:mutong(david)
+ * time:13:04
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hellow .git");
+    }
+}
